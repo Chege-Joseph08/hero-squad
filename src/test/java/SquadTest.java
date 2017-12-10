1dbed0fdaadf00e3a23c7d1038609a9a289326e9
JOSEPH CHEGE <josephchege08@gmail.com>
2017-12-10 23:14:04 +0300

    Modified SquadTest

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -5,28 +5,63 @@ public class SquadTest {
 
   @Test
   public void Squad_instantiatesCorrectly_true() {
-    Squad testSquad = new Squad("testSquad", "Yoga");
+    Squad testSquad = new Squad("testSquad", "poverty");
     assertEquals(true, testSquad instanceof Squad);
   }
 
   @Test
   public void Squad_instantiateswithName_testSquad() {
-    Squad testSquad = new Squad("testSquad", "Yoga");
+    Squad testSquad = new Squad("testSquad", "poverty");
     assertEquals("testSquad", testSquad.getName());
   }
 
   @Test
-  public void Squad_instantiateswithReason_poverty() {
-    Squad testSquad = new Squad("testSquad", "Yoga");
-    assertEquals("Yoga", testSquad.getCause());
+  public void Squad_instantiateswithCause_poverty() {
+    Squad testSquad = new Squad("testSquad", "poverty");
+    assertEquals("poverty", testSquad.getCause());
   }
+
+
 
   @Test
     public void getId_SquadsInstantiateWithAnId_1() {
      Squad.clear();
-     Squad testSquad = new Squad("testSquad", "Yoga");
+     Squad testSquad = new Squad("testSquad", "poverty");
     assertEquals(1, testSquad.getId());
   }
 
-  
+  @Test
+  public void find_returnsHedroWithSameId_testSquad2() {
+    Squad.clear();
+    Squad testSquad = new Squad("testSquad", "poverty");
+    Squad testSquad2 = new Squad("testSquad2", "hunger");
+    assertEquals(Squad.find(testSquad2.getId()), testSquad2);
   }
+
+  @Test
+  public void getHeroes_initiallyReturnsEmptyList_ArrayList() {
+    Squad testSquad = new Squad("testSquad", "poverty");
+    assertEquals(0, testSquad.getHeroes().size());
+  }
+  @Test
+   public void find_returnsNullWhenNoHeroFound_null() {
+     assertTrue(Squad.find(1000) == null);
+   }
+
+  @Test
+
+  public void clear_emptiesAllSquadsFromArrayList_0() {
+    Squad testSquad = new Squad("testSquad", "poverty");
+    Squad.clear();
+    assertEquals(0, Squad.all().size());
+  }
+
+  @Test
+  public void addHero_addsHeroToList_true() {
+    Squad testSquad = new Squad("testSquad", "poverty");
+    Hero newHero = new Hero("githeriMan", 30, "Super strength", "avocado");
+    testSquad.addHero(newHero);
+    assertTrue(testSquad.getHeroes().contains(newHero));
+  }
+
+}
